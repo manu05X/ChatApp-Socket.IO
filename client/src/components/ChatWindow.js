3db3fb5c6b095f68042fd46e07eb82034112f50f
MANISH KUMAR <36466808+manu05X@users.noreply.github.com>
2023-10-23 19:37:47 +0530

    making left and right align for message recieved and send

--- client/src/components/ChatWindow.js
+++ client/src/components/ChatWindow.js
@@ -4,18 +4,19 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
+import Card from '@mui/material/Card';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
+import { grey } from '@mui/material/colors';
 
 export default function ChatWindow() {
     const [socket, setSocket] = useState(null);
     //const socket = io("http://localhost:4000");
-    const [message, setMessage] = useState();
+    const [message, setMessage] = useState("");
     /*collect what ever we are sending from one windo to othwr and show it in UI*/
     //ir is an arry of empty string
     const [chat, setChat] = useState([]);
@@ -35,7 +36,7 @@ export default function ChatWindow() {
         socket.on("message-from-server", (data) => {
           //console.log("Message Recieved", data);
           //now set the chat
-          setChat((prev) => [...prev, data.message]);
+          setChat((prev) => [...prev, {message: data.message, received: true}]);
         });
       }, [socket]);
     
@@ -47,35 +48,49 @@ export default function ChatWindow() {
         {message} is the obj data that is sent
         */
         socket.emit('send-message', {message});
+        setChat((prev) => [...prev, {message, received: false}]);
         setMessage("");
       }
 
   return (
-    <>
-    <Box sx={{marginBottom:5}}>
-      {
-        chat.map((message)=>(
-          <Typography key={message}>{message}</Typography>
-        ))
-      }
-    </Box>
-      
-    <Box component="form" onSubmit={handleForm}>
-        <OutlinedInput
-          placeholder="Write your message"
-          size="small"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton type="submit" edge="end">
-                <SendIcon/>
-              </IconButton>
-            </InputAdornment>
-           }
-        />
+    <Box sx={{display:'flex', justifyContent:"center"}}>
+        <Card 
+        sx={{ 
+            padding:2, 
+            marginTop:10, 
+            width:"60%",
+            backgroundColor:"grey"
+            }}>
+            <Box sx={{ marginBottom:5 }}>
+            {
+                chat.map((data)=>(
+                <Typography 
+                sx={{textAlign: data.received ? "left" : "right"}} 
+                key={data.message}>
+                    {data.message}
+                </Typography>
+                ))
+            }
+            </Box>
+        
+            <Box component="form" onSubmit={handleForm}>
+                <OutlinedInput
+                sx={{backgroundColor:"white", width:"100%"}}
+                placeholder="Write your message"
+                size="small"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                endAdornment={
+                    <InputAdornment position="end">
+                    <IconButton type="submit" edge="end">
+                        <SendIcon/>
+                    </IconButton>
+                    </InputAdornment>
+                }
+                />
 
+            </Box>
+        </Card>
     </Box>
-    </>
   )
 }

5037ffeb3b2a00bd6f83698a3e46c019eeb23d1c
MANISH KUMAR <36466808+manu05X@users.noreply.github.com>
2023-10-25 17:27:23 +0530

    making Typing event for frontend

--- client/src/components/ChatWindow.js
+++ client/src/components/ChatWindow.js
@@ -1,8 +1,7 @@
 //rfc shorcut for templet
 import React from 'react'
-import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
-import Container from '@mui/material/Container';
+import InputLabel from '@mui/material/InputLabel';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import OutlinedInput from '@mui/material/OutlinedInput';
@@ -11,7 +10,6 @@ import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
-import { grey } from '@mui/material/colors';
 
 export default function ChatWindow() {
     const [socket, setSocket] = useState(null);
@@ -20,6 +18,8 @@ export default function ChatWindow() {
     /*collect what ever we are sending from one windo to othwr and show it in UI*/
     //ir is an arry of empty string
     const [chat, setChat] = useState([]);
+    //Typing event
+    const [typing, setTyping] = useState(false);
 
     useEffect(()=>{
         setSocket(io("http://localhost:4000"));
@@ -36,10 +36,25 @@ export default function ChatWindow() {
         socket.on("message-from-server", (data) => {
           //console.log("Message Recieved", data);
           //now set the chat
+          // then set received true or false
           setChat((prev) => [...prev, {message: data.message, received: true}]);
         });
+
+        //4> typing-started-from-server from server recieved
+        socket.on("typing-started-from-server", () => 
+            //console.log("typing.....");
+            setTyping(true)
+          );
+
+          //4> typing-stoped-from-server from server recieved
+          socket.on("typing-stoped-from-server", () =>
+            //console.log("typing.....");
+            setTyping(false)
+          );
+
       }, [socket]);
-    
+
+      //handling message event
       function handleForm(e){ 
         e.preventDefault();
         //console.log(message);
@@ -48,8 +63,27 @@ export default function ChatWindow() {
         {message} is the obj data that is sent
         */
         socket.emit('send-message', {message});
+        // then set received false as we are sending it
         setChat((prev) => [...prev, {message, received: false}]);
         setMessage("");
+      }
+
+      const [typingTimeout, settypingTimeout] = useState(null);
+
+      //Typing event is triggered that need to be handled 
+      function handleInput(e){
+        setMessage(e.target.value);
+        //1> sending event from frontend to backend
+        socket.emit("typing-started");
+
+        if(typingTimeout) clearTimeout(typingTimeout);
+
+        settypingTimeout(
+            setTimeout(()=>{
+                //1
+                socket.emit("typing-stoped");
+            },1000)
+        );
       }
 
   return (
@@ -74,12 +108,20 @@ export default function ChatWindow() {
             </Box>
         
             <Box component="form" onSubmit={handleForm}>
+            {typing && (
+                <InputLabel sx={{color:"white"}} shrink htmlFor="message-input">
+                    Typing...
+                </InputLabel>
+            )}
+            
                 <OutlinedInput
                 sx={{backgroundColor:"white", width:"100%"}}
                 placeholder="Write your message"
                 size="small"
+                fullWidth
+                id="message-input"
                 value={message}
-                onChange={(e) => setMessage(e.target.value)}
+                onChange={handleInput}
                 endAdornment={
                     <InputAdornment position="end">
                     <IconButton type="submit" edge="end">
